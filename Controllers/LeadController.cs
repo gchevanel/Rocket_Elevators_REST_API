73b9fc14551c5d8364e4347732ffc27669b22a29
Gchevanel <gchevanel@hotmail.com>
2019-04-24 17:42:33 -0400

    ajusted leads for working with all leads

--- Controllers/LeadController.cs
+++ Controllers/LeadController.cs
@@ -12,19 +12,34 @@ using MySql.Data.MySqlClient;
 
 using RocketElevatorApi.Models;
 
-namespace RocketElevatorApi.Controllers {
+namespace RocketElevatorApi.Controllers
+{
     [Route("api/[controller]")]
     [ApiController]
-    public class LeadController : ControllerBase {
+    public class LeadController : ControllerBase
+    {
         private readonly RocketElevatorContext _context;
 
-        public LeadController(RocketElevatorContext context) {
+        public LeadController(RocketElevatorContext context)
+        {
             _context = context;
         }
 
         // GET: api/lead
+
+        [HttpGet]
+        public IEnumerable<Lead> GetLeadss()
+        {
+            IQueryable<Lead> Leads =
+            from le in _context.Leads
+
+            select le;
+
+            return Leads.ToList();
+        }
         [HttpGet("recent")]
-        public IEnumerable<Lead> GetLeads() {
+        public IEnumerable<Lead> GetLeads()
+        {
             IQueryable<Lead> Leads =
             from le in _context.Leads
             where le.customer_id == null && le.created_at >= DateTime.Now.AddDays(-30)
